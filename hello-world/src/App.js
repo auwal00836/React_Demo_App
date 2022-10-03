cb19625fd0fb90612b901b2c418b38a1fa09bcb2
auwal00836 <auwalshikutu@gmail.com>
2022-10-03 12:20:54 +0100

    first commit

---
+++ hello-world/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './component/Greetings';
+import Greetings from './component/Greetings';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Greetings/>
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-world/src/component/Greetings.js
@@ -0,0 +1,10 @@
+
+//const Greetings = () => <h2>hello testing</h2>
+function Greetings(){
+    return(
+        <h1>React is {5 + 5} times better with JSX</h1>
+    ) 
+}
+
+
+export default Greetings;
